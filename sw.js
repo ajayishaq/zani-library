4f4049d930abeeac4e8c60c1308d5986dd94d786
ajayishaq <ishaqmail09@gmail.com>
2025-08-21 06:03:02 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+onst CACHE_NAME = 'zani-library-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add your CSS and JS files here when separate
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
